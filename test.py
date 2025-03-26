c9e0090f5203bca67792dfd3ff3445aa29d0e3de
nobody911 <kroxgod@LAPTOP-3E5FQLAH.>
2025-03-26 16:34:53 +0530

    OOP concepts

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
-class Library:
-    def __init__(self, name):
-        self.name = name
-        self.books = []
-    def add_book(self, book):
-        self.books.append(book)
-    def list_books(self):
-        return [book for book in self.books]
-class Book:
-    def __init__(self, name, author):
-        self.name = name
-        self.author = author
+class Engine:
+    def __init__(self, horsepower):
+        self.horsepower = horsepower
+class Wheel:
+    def __init__(self, size):
+        self.size = size
+class Car:
+    def __init__(self, horse_power, wheel_size, model, year):
+        self.model = model
+        self.year = year
+        self.engine = Engine(horsepower=horse_power)
+        self.wheels = [Wheel(wheel_size) for wheel in range(4)]
+    def print_wheel(self):
+        return f"{self.engine.horsepower}, {self.model}, {self.year}, {self.wheels[0].size}"
 
-library = Library("New York State Library")
+car1 = Car(horse_power=200, wheel_size=18, model="Tesla", year=2021)
+car2 = Car(horse_power=600, wheel_size=14, model="Mercedes", year=2024)
 
-book1 = Book("Jungle Book", "J.K Rowling")
-book1 = Book("The Invisible Man", "Stephen King")
-book1 = Book("Mein Kamph", "Adolf Hitler")
-
+print(car1.print_wheel())
+print(car2.print_wheel())
